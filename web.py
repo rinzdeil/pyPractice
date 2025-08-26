e72345fe5b67d5259c68f6bf14fdca3b14f8627f
rinzdeil <dalesumande@gmail.com>
2025-08-27 03:24:08 +0800

    added complete function for todos

--- web.py
+++ web.py
@@ -2,22 +2,38 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+
 def add_todo():
     todo = st.session_state["new_todo"]  + "\n"
     todos.append(todo)
     functions.write_todos(todos)
     print(todo)
 
+# def complete_todo():
+#     for todo in todos:
+#         todo_state = st.session_state[todo]
+#         if todo_state == "true":
+#             todos.remove(todo)
+
+#             functions.write_todos(todos)
+
 
 st.title("My TODO App")
 st.subheader("This is my todo APP")
 st.write("This is just a simple app for Python practice")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a todo",
               placeholder="Add a new todo...",
               key="new_todo",
               on_change=add_todo
               )
+
+st.session_state
