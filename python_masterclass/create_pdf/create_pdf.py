f484469a3be7fc7364331399f92fe52856a18ba1
rinzdeil <140869339+rinzdeil@users.noreply.github.com>
2025-09-11 14:05:16 +0000

    Added page number in footer and lines for each page

--- python_masterclass/create_pdf/create_pdf.py
+++ python_masterclass/create_pdf/create_pdf.py
@@ -5,20 +5,31 @@ df = pd.read_csv('python_masterclass/create_pdf/pdf_topics.csv')
 
 pdf = FPDF(orientation='P', unit='pt', format='A4')
 
-
-def create_page(topic):
+def create_page(topic, page_num):
   pdf.add_page()
   pdf.set_font(family='Helvetica', style='B', size=24)
   pdf.set_text_color(100, 100, 100)
-  pdf.cell(w=0, h=36, txt=topic, ln=1, border='B')
+  pdf.cell(w=0, h=36, txt=f"{page_num}. " + topic, ln=1, border='B')
 
+  for x in range(42):
+    pdf.cell(w=0, h=16, txt='', ln=1, border='B')
+
+def footer():
+  current_page = pdf.page_no()
+  total_pages = len(df)
+  pdf.ln(10)
+  pdf.set_font(family='Helvetica', style='I', size=12)
+  pdf.set_text_color(100, 100, 100)
+  pdf.cell(w=0, h=10, txt=f'p. {current_page}/{total_pages}', align='R')
 
 for index, row in df.iterrows():
-  num_pages = int(row['Pages'])
+  pages = int(row['Pages'])
+  page_num = int(row['Order'])
   topic = row['Topic']
 
-  for _ in range(num_pages):
-    create_page(topic)
+  for i in range(pages):
+    create_page(topic, page_num)
+    footer()
 
 pdf.output('python_masterclass/create_pdf/output.pdf')
 
