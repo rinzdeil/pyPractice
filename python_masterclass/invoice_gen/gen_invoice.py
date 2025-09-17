8c879fdd55a0ed06429b5ada90bb5f815f75bd6a
rinzdeil <dalesumande@gmail.com>
2025-09-18 02:57:58 +0800

    Added column headers, net total. Fixed column alignment. Day 26

--- python_masterclass/invoice_gen/gen_invoice.py
+++ python_masterclass/invoice_gen/gen_invoice.py
@@ -27,27 +27,32 @@ for file in excel_files:
 
   df = pd.read_excel(file, sheet_name='Sheet 1')
 
-  columns = [item.replace("_", " ") for item in df.columns]
+  columns = [item.replace("_", " ").title() for item in df.columns]
 
-  for i in range(len(columns)):
-    pdf.set_font(family='Helvetica', style='B', size=10)
-    pdf.cell(col_width, row_height, txt=columns[i], border=1, align='C')
-  
+
+  pdf.set_font(family='Helvetica', style='B', size=9)
+  pdf.cell(30, row_height, txt=str(columns[0]), border=1, align='C')
+  pdf.cell(50, row_height, txt=str(columns[1]), border=1, align='C')
+  pdf.cell(40, row_height, txt=str(columns[2]), border=1, align='C')
+  pdf.cell(30, row_height, txt=str(columns[3]), border=1, align='C')
+  pdf.cell(30, row_height, txt=str(columns[4]), border=1, align='C', ln=1)
+
   for index, row in df.iterrows():
     pdf.set_font(family='Helvetica', style='', size=8)
-    pdf.cell(20, row_height, txt=str(row['product_id']), border=1, align='C')
-    pdf.cell(70, row_height, txt=str(row['product_name']), border=1)
-    pdf.cell(10, row_height, txt=str(row['amount_purchased']), border=1)
-    pdf.cell(15, row_height, txt=str(row['price_per_unit']), border=1)
-    pdf.cell(15, row_height, txt=str(row['total_price']), border=1)
+    pdf.cell(30, row_height, txt=str(row['product_id']), border=1, align='C')
+    pdf.cell(50, row_height, txt=str(row['product_name']), border=1, align='C')
+    pdf.cell(40, row_height, txt=str(row['amount_purchased']), border=1, align='C')
+    pdf.cell(30, row_height, txt=str(row['price_per_unit']), border=1, align='C')
+    pdf.cell(30, row_height, txt=str(row['total_price']), border=1, align='C')
     pdf.ln(row_height)
-    
-  #   print(index, row)
-    # for item in row:
-    #   print(item['product_id'])
-    # print(type(item))
-    # pdf.ln(row_height)
 
+  total_cost = df['total_price'].sum()
+  pdf.set_fill_color(230, 230, 230)
+  pdf.set_font(family='Helvetica', style='B', size=10)
+  pdf.cell(150, row_height, txt="  Net Total", border=1, fill=1, align='L')
+  pdf.cell(30, row_height, txt=str(total_cost), border=1, align='C', fill=1)
+
+  print("PDF Created")
   pdf.output(f"python_masterclass/invoice_gen/{filename}.pdf")
 
 # print(excel_files)
