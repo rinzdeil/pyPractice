764e57fc3db1aca42021e0d038faf6dae301dec6
sumandew <dalesumande@gmail.com>
2025-10-17 21:52:33 +0800

    Words API, Student Project Day 29

--- python_masterclass/weather_api/app.py
+++ python_masterclass/weather_api/app.py
@@ -4,19 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
 
-@app.route('/stations/')
-def index():
-    return render_template('stations.html') 
+# @app.route('/api/v1/<station>/<date>')
+# def station():
+#     temperature = 23
+#     return str(temperature)
 
-@app.route('/words/')
-def index():
-    return render_template('words.html') 
+@app.route('/api/v1/<word>')
+def api(word):
+    definition = word.upper()
+    dict_response = {
+        "word": word,
+        "definition": definition
+    }
+    return dict_response
 
-@app.route("/api/v1/<station>/<date>")
-def api(station, date):
-    return render_template("api.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
